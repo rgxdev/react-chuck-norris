1fdf2b348889352635d74bfff384c474f92344f3
rgxdev <contact@d-aaron.dev>
2024-12-05 16:54:34 +0100

    Init

--- src/firebase/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, indexedDBLocalPersistence } from "firebase/auth";
-import { firebaseConfig } from "./firebaseConfig";
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
-auth.setPersistence(indexedDBLocalPersistence);
